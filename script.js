3d1115d7c368a308627c6ed5e810df42ee9b2d31
Caleb Stout <hisurfingc@yahoo.com>
2020-08-31 22:38:36 -0700

    created dark/light functions, corrected image colors of an svg file

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const toggleText = document.getElementsByClassName('toggle-text');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
+
+// Dark-Light Images
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`
+    image2.src = `img/undraw_feeling_proud_${color}.svg`
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`
+}
 
 //Dark Mode Styles
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[2].classList.add('fa-moon');
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
 }
 
 //Light Mode Styles
 function lightMode() {
-
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
 } 
 
 // Switch Theme Dynamically
