0e4d51dffb32689ecc425cc830943db3ab9dfb85
Caleb Stout <hisurfingc@yahoo.com>
2020-08-31 22:17:46 -0700

    corrected syntax error

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementsByClassName('toggle-text');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+//Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[2].classList.add('fa-moon');
+}
+
+//Light Mode Styles
+function lightMode() {
+
+} 
 
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute("data-theme", "dark");
+        darkMode();
     } else {
         document.documentElement.setAttribute("data-theme", "light");
+        lightMode();
     }
 }
 
